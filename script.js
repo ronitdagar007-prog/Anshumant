6439383ea2ba7e925020ebac54a4f2c7a17be4dc
ronitdagar007-prog <ronitdagar007@gmail.com>
2025-09-04 19:55:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Simple form handling (frontend only)
+document.getElementById("contactForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Thank you! Your message has been submitted.");
+  this.reset();
+});
